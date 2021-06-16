417f28e1f39b8d344cad130533b8b1c5153974f4
Alexander <alexanderdeveloper@gmail.com>
2021-06-16 09:52:36 +0300

    fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
 import { Cards } from './components/cards/Cards'
+
 const App = () => {
-  let cards = [{
+  const cards = [{
     id: 2523353,
     text: 'Snow Tender Ice Cream',
     price: '$410.00'
   },
   {
-    id: 2523353,
+    id: 2523245,
     text: 'Snow Tender Ice Cream',
     price: '$391.00'
   },
   {
-    id: 2523353,
+    id: 2587656,
     text: 'Snow Tender Ice Cream',
     price: '$501.00'
   },
   {
-    id: 2523353,
+    id: 5466587,
     text: 'Snow Tender Ice Cream',
     price: '$726.00'
   },
   {
-    id: 2523353,
+    id: 3546765,
     text: 'Snow Tender Ice Cream',
     price: '$111.00'
   },
   {
-    id: 2523353,
+    id: 8796753,
     text: 'Snow Tender Ice Cream',
     price: '$243.00'
   }]
